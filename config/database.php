f57885251fcc8fb44b7c9a26292008a53542822d
Alok Bhagat <bhagatalok35@gmail.com>
2026-07-04 10:07:21 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,1 +1,20 @@
 
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "security_incident_db";
+
+$conn = mysqli_connect(
+    $host,
+    $username,
+    $password,
+    $database
+);
+
+if (!$conn) {
+    die("Database Connection Failed: " . mysqli_connect_error());
+}
+
+?>
